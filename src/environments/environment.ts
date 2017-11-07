a6ad6dc01f13909954a25df243717610cdbccef5
Jörg <joerg.sesterhenn@gmail.com>
2017-11-08 00:23:49 +0100

    initial commit of example project

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAuPHeKnjzhn-oCefJqm7zon1mnhj0ZrHQ',
+    authDomain: 'chickenpi-server.firebaseapp.com',
+    databaseURL: 'https://chickenpi-server.firebaseio.com',
+    projectId: 'chickenpi-server',
+    storageBucket: 'chickenpi-server.appspot.com',
+    messagingSenderId: '557371977224'
+  }
+};
