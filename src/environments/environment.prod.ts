249d76b2a6d938934b80196ed150079aa01f1f8d
Jörg <joerg.sesterhenn@gmail.com>
2017-11-08 01:01:33 +0100

    added config for prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAuPHeKnjzhn-oCefJqm7zon1mnhj0ZrHQ',
+    authDomain: 'chickenpi-server.firebaseapp.com',
+    databaseURL: 'https://chickenpi-server.firebaseio.com',
+    projectId: 'chickenpi-server',
+    storageBucket: 'chickenpi-server.appspot.com',
+    messagingSenderId: '557371977224'
+  }
 };
