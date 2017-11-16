a35383fed482647e6118b8e58b6dbfc3821f74f8
Jörg <joerg.sesterhenn@gmail.com>
2017-11-16 01:57:05 +0100

    multiple platforms

--- protractor.conf.js
+++ protractor.conf.js
@@ -6,14 +6,50 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 120000,
   specs: [ './e2e/**/*.e2e-spec.ts' ],
-  sauceUser = process.env.SAUCE_USERNAME,
-  sauceKey = process.env.SAUCE_ACCESS_KEY,
-  capabilities = {
+  sauceUser : process.env.SAUCE_USERNAME,
+  sauceKey : process.env.SAUCE_ACCESS_KEY,
+  
+  multiCapabilities: [{
     'browserName': 'chrome',
+    'platform': 'LINUX',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
     'name': 'Chickenpi App'
-  },
+  },{
+    'browserName': 'firefox',
+    'platform': 'LINUX',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Chickenpi App'
+  },{
+    'browserName': 'chrome',
+    'platform': 'WINDOWS',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Chickenpi App'
+  },{
+    'browserName': 'firefox',
+    'platform': 'WINDOWS',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Chickenpi App'
+  },{
+    'browserName': 'chrome',
+    'platformName': 'Android',
+    'platformVersion': '7.0',
+    'deviceName': 'Android Emulator',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Chickenpi App'
+  },{
+    'browserName': 'safari',
+    'platformName': 'iOS',
+    'platformVersion': '7.1',
+    'deviceName': 'iPhone Simulator',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Chickenpi App'
+  }],
   directConnect: false,
   baseUrl: 'http://localhost:5600/',
   framework: 'jasmine',
