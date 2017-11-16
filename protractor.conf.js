8489d686f47147c2f4934b529d7d01f3763e44a1
Jörg <joerg.sesterhenn@gmail.com>
2017-11-16 01:27:43 +0100

    fix config

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,28 +5,9 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 120000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
-
-  multiCapabilities: [
-  {
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Shepherd DEV functional',
-    'chromeOptions': {
-      'args': ['--no-sandbox', '--test-type=browser'],
-      'prefs': {
-        'download': {
-          'prompt_for_download': false,
-          'default_directory': '/tmp'
-        }
-      }
-    }
-  }],
-  
-  directConnect: true,
+  specs: [ './e2e/**/*.e2e-spec.ts' ],
+  multiCapabilities: [ {'browserName': 'chrome'} ],
+  directConnect: false,
   baseUrl: 'http://localhost:5600/',
   framework: 'jasmine',
   jasmineNodeOpts: {
@@ -41,3 +22,14 @@ exports.config = {
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
+
+if (process.env.TRAVIS) {
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+  config.capabilities = {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Chickenpi App'
+  };
+}
