da1d006e84ee171db3d7ff9e5d5e95d2f58ed472
Jörg <joerg.sesterhenn@gmail.com>
2017-11-16 23:46:34 +0100

    changed travis config

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,57 +4,48 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 120000,
+  allScriptsTimeout: 600000,
   specs: [ './e2e/**/*.e2e-spec.ts' ],
   sauceUser : process.env.SAUCE_USERNAME,
   sauceKey : process.env.SAUCE_ACCESS_KEY,
+  build : process.env.TRAVIS_BUILD_NUMBER,
+  name: 'Chickenpi App',
+
   multiCapabilities: [{
     'browserName': 'chrome',
     'platform': 'LINUX',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Chickenpi App'
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
   },{
     'browserName': 'firefox',
     'platform': 'LINUX',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Chickenpi App'
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
   },{
     'browserName': 'chrome',
     'platform': 'WINDOWS',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Chickenpi App'
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
   },{
     'browserName': 'firefox',
     'platform': 'WINDOWS',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Chickenpi App'
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
   },{
     'browserName': 'chrome',
     'platformName': 'Android',
     'platformVersion': '7.0',
     'deviceName': 'Android Emulator',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Chickenpi App'
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
   },{
     'browserName': 'safari',
     'platformName': 'iOS',
     'platformVersion': '7.1',
     'deviceName': 'iPhone Simulator',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Chickenpi App'
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
   }],
   directConnect: false,
   baseUrl: 'http://localhost:5600/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 40000,
+    defaultTimeoutInterval: 120000,
     print: function() {}
   },
   onPrepare() {
